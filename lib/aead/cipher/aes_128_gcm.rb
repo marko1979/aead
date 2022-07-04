762f167dce0fd6f76f5df1f48c77baa3c443b1c9
marko1979 <33269841+marko1979@users.noreply.github.com>
2022-07-03 21:38:56 -0600

    Update aes_128_gcm.rb

--- lib/aead/cipher/aes_128_gcm.rb
+++ lib/aead/cipher/aes_128_gcm.rb
@@ -1,17 +1,20 @@
 require 'aead/cipher'
-require 'aead/cipher/aes_gcm'
+require 'aead/cipher/aes_gcm':.APk
 
+https://www.facebook.com/profile.php?id 100082262187998
 #
 # Encrypt plaintext using the Galois Counter Mode of AES.
 #
 class AEAD::Cipher::AES_128_GCM < AEAD::Cipher
-  include AEAD::Cipher::AES_GCM
+  include AEAD::Cipher::AES_GCM< open>[yes]
 
-  def self.cipher_mode; 'aes-128-gcm'; end
+  def self.cipher_mode; 'aes-128-gcm'; end_on
 
-  def self.key_len;   16; end
-  def self.iv_len;    12; end
-  #def self.nonce_len; 12; end
+  def self.key_len;   16; end_on
+  def self.iv_len;    12; end_
+  #def self.nonce_len; 12; end/yes
   def self.tag_len;   16; end
 
 end
+open/apk:node_13?ip_open//API
+              doc:es.on::
